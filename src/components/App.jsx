9d93b2a5780fbdc4bbffc4ca7f558def0b2bec9a
LDanchenko <2l-u-b-o-v@ukr.net>
2022-05-20 19:59:26 +0300

    Added method for check if contact exist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,12 @@ import { ContactList } from './ContactList';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -15,13 +20,22 @@ class App extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (name, number) => {
-    this.setState(prevState => {
-      const id = nanoid();
-      return {
-        contacts: [...prevState.contacts, { id, name, number }],
-      };
-    });
+  addContact = (name, number) => {
+    const nameNormalized = name.toLowerCase();
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === nameNormalized
+      )
+    ) {
+      alert(name + ' is already in the contacts.');
+    } else {
+      this.setState(prevState => {
+        const id = nanoid();
+        return {
+          contacts: [...prevState.contacts, { id, name, number }],
+        };
+      });
+    }
   };
 
   render() {
@@ -30,7 +44,7 @@ class App extends Component {
     return (
       <div className={style.container}>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.handleSubmit} />
+        <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
         {contacts.length > 0 && (
           <>
