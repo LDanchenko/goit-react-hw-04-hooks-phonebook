b4f17d968d4a3879f26333e06afa2cdcf390683e
LDanchenko <2l-u-b-o-v@ukr.net>
2022-05-20 12:01:16 +0300

    Fixed filter for contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,7 @@ const INITIAL_STATE = {
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     name: '',
     number: '',
     filter: '',
@@ -48,7 +43,6 @@ class App extends Component {
   render() {
     const { contacts, name, number, filter } = this.state;
     const filterNormalized = filter.toLowerCase();
-    console.log(filterNormalized);
     return (
       <div className={style.container}>
         <h2>Phonebook</h2>
