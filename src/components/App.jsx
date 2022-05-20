339d23e15f6e012812e5fd8dcce977d9b961d6be
LDanchenko <2l-u-b-o-v@ukr.net>
2022-05-20 22:55:47 +0300

    Added method for deleting contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { ContactList } from './ContactList';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -38,6 +33,12 @@ class App extends Component {
     }
   };
 
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
   render() {
     const { contacts, filter } = this.state;
     const filterNormalized = filter.toLowerCase();
@@ -52,7 +53,11 @@ class App extends Component {
               value={this.state.filter}
               onChange={this.handleInputChange}
             />
-            <ContactList contacts={contacts} filter={filterNormalized} />
+            <ContactList
+              contacts={contacts}
+              filter={filterNormalized}
+              onDeleteItem={this.deleteContact}
+            />
           </>
         )}
       </div>
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,7 +1,7 @@
 import style from './ContactList.module.css';
 import PropTypes from 'prop-types';
 
-const ContactList = ({ contacts, filter }) => {
+const ContactList = ({ contacts, filter, onDeleteItem }) => {
   return (
     <ul className={style.list}>
       {contacts
@@ -9,6 +9,12 @@ const ContactList = ({ contacts, filter }) => {
         .map(contact => (
           <li key={contact.id}>
             {contact.name}: {contact.number}
+            <button
+              className={style.button}
+              onClick={() => onDeleteItem(contact.id)}
+            >
+              Delete
+            </button>
           </li>
         ))}
     </ul>
