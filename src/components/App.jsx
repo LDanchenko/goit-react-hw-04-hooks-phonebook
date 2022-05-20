0ddb6010595f7156abb75579e019dae9f85e3489
LDanchenko <2l-u-b-o-v@ukr.net>
2022-05-20 11:32:16 +0300

    Created input for adding phone number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,23 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import style from './App.module.css';
 
+const INITIAL_STATE = {
+  name: '',
+  number: '',
+};
+
 class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
-  handleInputChange = event => {
-    this.setState({ name: event.target.value });
+  numberId = nanoid();
+  nameId = nanoid();
+
+  handleInputChange = name => event => {
+    this.setState({ [name]: event.target.value });
   };
 
   handleSubmit = event => {
@@ -17,35 +26,52 @@ class App extends Component {
     this.setState(prevState => {
       const id = nanoid();
       return {
-        contacts: [...prevState.contacts, { id, name: this.state.name }],
+        contacts: [
+          ...prevState.contacts,
+          { id, name: this.state.name, number: this.state.number },
+        ],
       };
     });
     this.reset();
   };
 
   reset = () => {
-    this.setState({ name: '' });
+    this.setState({ ...INITIAL_STATE });
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, name, number } = this.state;
 
     return (
       <div className={style.container}>
         <h2>Phonebook</h2>
         <form className={style.form} onSubmit={this.handleSubmit}>
-          <label className={style.label} htmlFor="name">
-            Name{' '}
+          <label className={style.label} htmlFor={this.nameId}>
+            Name
           </label>
           <input
             type="text"
             name="name"
-            id="name"
+            id={this.nameId}
             value={name}
             className={style.input}
-            onChange={this.handleInputChange}
+            onChange={this.handleInputChange('name')}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <label className={style.label} htmlFor={this.numberId}>
+            Number
+          </label>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            id={this.numberId}
+            className={style.input}
+            onChange={this.handleInputChange('number')}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
           <button className={style.button} type="submit">
@@ -56,7 +82,9 @@ class App extends Component {
         {contacts.length > 0 && (
           <ul className={style.list}>
             {contacts.map(contact => (
-              <li key={contact.id}>{contact.name}</li>
+              <li key={contact.id}>
+                {contact.name}: {contact.number}
+              </li>
             ))}
           </ul>
         )}
